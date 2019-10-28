3188f10ea5ee6d99a7cc14d91c8fc73bcb7d7163
Murat Kumykov <mkumykov@blackducksoftware.com>
2019-10-27 21:36:16 -0400

    OpenShift compatible user ID added, static configuration

--- container/docker-entrypoint.sh
+++ container/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 # 
 
 LOGDIR=/var/lib/logstash/data
-CONFIGFILE=/etc/rsyslog.d/send-to-remote.conf
+#CONFIGFILE=/etc/rsyslog.d/send-to-remote.conf
 
 if [ ! -d $LOGDIR ]
 then
@@ -10,16 +10,16 @@ then
   exit 1
 fi
 
-LOGFILES=$( find /var/lib/logstash/data/ -name \*\.log | \
-            sed 's/[0-9]\{4\}-[0-9][0-9]-[0-9][0-9]\.log/*.log/' | \
-            sort | uniq )
-
-echo $LOGDIR
-echo "$LOGFILES" | awk -f /usr/local/lib/process.awk \
-                   -v host=$REMOTE_SYSLOG_HOST \
-                   -v port=$REMOTE_SYSLOG_PORT \
-                   -v protocol=$REMOTE_SYSLOG_PROTOCOL \
-                   > $CONFIGFILE
+#LOGFILES=$( find /var/lib/logstash/data/ -name \*\.log | \
+#            sed 's/[0-9]\{4\}-[0-9][0-9]-[0-9][0-9]\.log/*.log/' | \
+#            sort | uniq )
+#
+#echo $LOGDIR
+#echo "$LOGFILES" | awk -f /usr/local/lib/process.awk \
+#                   -v host=$REMOTE_SYSLOG_HOST \
+#                   -v port=$REMOTE_SYSLOG_PORT \
+#                   -v protocol=$REMOTE_SYSLOG_PROTOCOL \
+#                   > $CONFIGFILE
 
 
 /usr/sbin/rsyslogd -n -i /var/run/rsyslogd/rsyslogd.pid ${LOGSHIPPER_OPTS}
